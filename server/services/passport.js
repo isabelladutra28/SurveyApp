aa67acd13612ac461c1810bce62c0226580a2c8e
Isabella Dutra <isabelladutrabh@gmail.com>
2023-06-27 20:02:42 +0800

    changing proxy

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,7 +6,8 @@ passport.use(
     new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL:"/auth/google/callback"
+    callbackURL:"/auth/google/callback",
+    proxy: true
 }, (accessToken, refreshToken, profile) => {
     console.log('access token', accessToken);
     console.log('refresh token', refreshToken);
